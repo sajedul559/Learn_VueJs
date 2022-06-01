de69be2bb1fafde44d4c945b5ae1d73b04e1f259
sajedul <98393471+sajedul43@users.noreply.github.com>
2022-06-01 22:18:22 +0600

    learn methods

--- class_1/app.js
+++ class_1/app.js
@@ -1,8 +1,16 @@
   new Vue({
    el:'#app',
    data:{
-       name: 'Learn Hunter',
+       name: 'Sajedul Islam khairul',
        
+   },
+   methods:{
+       Myage(){
+           return 'learn sajedul';
+       },
+       Myname(age){
+        return `${this.name} with suly, my age is ${age}`
+    }
    }
 
 })
